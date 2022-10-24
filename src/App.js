289ca0d0cb9eef788fffcd9b46069618a7d34b68
Laura Clobochar Pereira <clo.bochar_@hotmail.com>
2022-10-24 09:25:28 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  let toDos = [
+    {
+      id: "1",
+      todo: "buy milk",
+    },
+    {
+      id: "2",
+      todo: "buy butter",
+    },
+    {
+      id: "3",
+      todo: "pay bill",
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
